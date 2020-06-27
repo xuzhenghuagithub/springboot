2394fb05da18cd3816754939c483e103a9a662ef
xuzhenghua <291493459@qq.com>
2020-06-27 17:29:21 +0800

    自动装配

--- springboot-demo/src/main/java/com/cnpc/demo/DemoApplication.java
+++ springboot-demo/src/main/java/com/cnpc/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.cnpc.demo;
 
+import com.cnpc.MyCoreBean;
 import com.cnpc.config1.MyRedisTemplate;
 import com.cnpc.config2.MySqlSessionFactory;
 import com.cnpc.importselector.MyEnableConfiguration;
@@ -15,10 +16,22 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class DemoApplication {
 
+    /**
+     * 自动装配
+     * 利用AutoConfigurationImportSelector和EnableAutoConfiguration将
+     * 引入的springboot-autoconfiguration-1.0-SHAPSHOT.JAR中的META-INF路径下的spring。factories文件中的配置类加载到IOC容器中
+     * 自动装载MyCoreConfiguration配置类中MyCoreBean
+     * @param args
+     */
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
         System.out.println(context.getBean(MyRedisTemplate.class));
         System.out.println(context.getBean(MySqlSessionFactory.class));
+
+        //自动装配
+        MyCoreBean bean = context.getBean(MyCoreBean.class);
+        System.out.println(bean);
+        bean.study();
     }
 
 }
